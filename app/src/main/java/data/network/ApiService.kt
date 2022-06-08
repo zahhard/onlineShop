036e60e62e7698daa85a9f13717f2605f5057933
zahhard <za.darardoust@gmail.com>
2022-06-08 22:16:39 +0430

    orderBy fun added to apiService

--- app/src/main/java/data/network/ApiService.kt
+++ app/src/main/java/data/network/ApiService.kt
@@ -1,6 +1,8 @@
 package data.network
 
 import model.Category
+import model.Produce
+import model.ProduceItem
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,4 +13,12 @@ interface ApiService {
         @Query("consumer_key") consumerKey : String = NetworkParams.consumer_key,
         @Query("consumer_secret") consumerSecret : String = NetworkParams.consumer_secret
     ) : List<Category>
+
+
+    @GET("products")
+    suspend fun getProduceList(
+        @Query("orderby") orderby : String ,
+        @Query("consumer_key") consumerKey : String = NetworkParams.consumer_key,
+        @Query("consumer_secret") consumerSecret : String = NetworkParams.consumer_secret,
+    ) : List<ProduceItem>
 }
