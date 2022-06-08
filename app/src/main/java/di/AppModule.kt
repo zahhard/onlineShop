90139f46a61c8578a5757f0cfd1ec2916fe2a216
zahhard <za.darardoust@gmail.com>
2022-06-08 11:16:58 +0430

    AppModule.kt added for use hilt

--- app/src/main/java/di/AppModule.kt
+++ app/src/main/java/di/AppModule.kt
@@ -1,4 +1,60 @@
 package di
 
-class AppModule {
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import data.network.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi, client: OkHttpClient):Retrofit{
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(client)
+            .build()
+        return retrofit
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi{
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        return moshi
+    }
+
+    @Singleton
+    @Provides
+    fun provideClient(): OkHttpClient{
+        val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .connectTimeout(50, TimeUnit.SECONDS)
+            .build()
+        return client
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit) : ApiService {
+        val apiService = retrofit.create(ApiService::class.java)
+        return apiService
+    }
 }
