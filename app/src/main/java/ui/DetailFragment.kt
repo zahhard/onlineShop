6ca6a247826b0cb1fb8235a9af0aa14f524eda17
zahhard <za.darardoust@gmail.com>
2022-06-11 22:31:23 +0430

    feat : category showed on detale fragment

--- app/src/main/java/ui/DetailFragment.kt
+++ app/src/main/java/ui/DetailFragment.kt
@@ -1,21 +1,16 @@
 package ui
 
-import adapter.CategoryAdapter
 import adapter.SliderAdapter
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.helper.widget.Carousel
-import androidx.fragment.app.FragmentActivity
+import androidx.core.text.HtmlCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.onlineshop.R
 import com.example.onlineshop.databinding.FragmentDetailBinding
-import com.example.onlineshop.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import model.ProduceItem
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
@@ -24,6 +19,7 @@ class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
     val detailViewModel : DetailViewModel by viewModels()
     var imageCont : Int = 0
+    var category : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +45,9 @@ class DetailFragment : Fragment() {
         detailViewModel.produceItemLiveData.observe(viewLifecycleOwner) {
             if (it != null) {
                 binding.tvDetailName.text = it.name
-                binding.tvDetailReview.text = it.price + " تومان "
+//                binding.tvDetailReview.text = it.price + " تومان "
+                binding.tvDetailReview.text =  HtmlCompat.fromHtml(it.description , HtmlCompat.FROM_HTML_MODE_LEGACY)
+
                 imageCont = it.images.size
 
                 for (image in it.images){
@@ -59,9 +57,17 @@ class DetailFragment : Fragment() {
                 }
 
                 binding.viewPagerImageSlider.adapter = SliderAdapter(this, urlList, binding.viewPagerImageSlider )
+                binding.rating.text = it.average_rating.toString()
+                getCategoryList(it)
+                binding.tvCategoryList.text = category
             }
         }
+    }
 
-
-
-    }}
+    private fun getCategoryList(produceItem: ProduceItem) {
+        for (itemCategory in produceItem.categories){
+            category += itemCategory.name
+            category += " / "
+        }
+    }
+}
