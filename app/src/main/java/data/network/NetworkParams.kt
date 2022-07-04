0a93a573a9ecd79b3dbf7a5b946989e7d5644904
zahhard <za.darardoust@gmail.com>
2022-07-04 16:06:40 +0430

    imp : navigate between loginFrament and detail fragment

--- app/src/main/java/data/network/NetworkParams.kt
+++ app/src/main/java/data/network/NetworkParams.kt
@@ -7,8 +7,8 @@ class NetworkParams {
     companion object {
         const val BASE_URL = "https://woocommerce.maktabsharif.ir/wp-json/wc/v3/"
         const val API_KEY = "b8fb74a7f7ebe3f2402f6de80059d5a5"
-        const val consumer_key = "ck_6b55bb0ff3ea0b7bf4c0aa879af50061964ce38f"
-        const val consumer_secret = "cs_9b09e5125acffdd27bbe72843ced49db5f8bffb4"
+        const val consumer_key = "ck_63f4c52da932ddad1570283b31f3c96c4bd9fd6f"
+        const val consumer_secret = "cs_294e7de35430398f323b43c21dd1b29f67b5370b"
         var  colorId = 0
     }
 }
