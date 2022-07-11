cf9f43a9635cb18b8021f6325ae2adc014ae1b5d
zahhard <za.darardoust@gmail.com>
2022-07-11 19:37:21 +0430

    feat : email property added to CommentsItem.kt

--- app/src/main/java/com/example/onlineshop/model/CommentsItem.kt
+++ app/src/main/java/com/example/onlineshop/model/CommentsItem.kt
@@ -8,11 +8,11 @@ data class CommentsItem(
 //    val date_created_gmt: String,
     @Json(name = "id")val id: Int,
     @Json(name = "product_id")val product_id: Int,
-    @Json(name = "rating")val rating: Int,
+    @Json(name = "rating")val rating: String,
     @Json(name = "review")val review: String,
     @Json(name = "reviewer")val reviewer: String,
 //    val reviewer_avatar_urls: ReviewerAvatarUrls,
-//    val reviewer_email: String,
+    @Json(name = "reviewer_email") val reviewer_email: String,
 //    val status: String,
 //    val verified: Boolean
 )
