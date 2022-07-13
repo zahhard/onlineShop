6d435f8e70fc7e75a8e2845af0e8e37342fa1f98
zahhard <za.darardoust@gmail.com>
2022-07-13 18:04:39 +0430

    imp : code get cleaned

--- app/src/main/java/com/example/onlineshop/ui/cart/CartFragment.kt
+++ app/src/main/java/com/example/onlineshop/ui/cart/CartFragment.kt
@@ -1,28 +1,42 @@
 package com.example.onlineshop.ui.cart
 
-import com.example.onlineshop.adapter.OrderAdapter
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.onlineshop.adapter.OrderAdapter
 import com.example.onlineshop.databinding.FragmentCartBinding
 import com.example.onlineshop.model.ApiStatus
-import dagger.hilt.android.AndroidEntryPoint
 import com.example.onlineshop.model.LineItem
+import com.example.onlineshop.model.OrderResponse
+import com.example.onlineshop.model.ProduceItem
+import dagger.hilt.android.AndroidEntryPoint
+import java.math.BigDecimal
+import kotlin.math.absoluteValue
 
 @AndroidEntryPoint
 class CartFragment : Fragment() {
 
     private lateinit var binding: FragmentCartBinding
-    val cartViewModel: CartViewModel by viewModels()
-    lateinit var sharedPreferences: SharedPreferences
+    private val cartViewModel: CartViewModel by viewModels()
+    private lateinit var sharedPreferences: SharedPreferences
+    private var produceList = MutableLiveData<List<ProduceItem>>(emptyList())
+    private var finalIdList = ArrayList<Int>()
+    private var quantityLista = ArrayList<Int>()
+    private var orderId = -1
+    var a = 0
+    var total :BigDecimal = a.toBigDecimal()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,36 +53,139 @@ class CartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        cartViewModel.status.observe(viewLifecycleOwner){
-            if (it == ApiStatus.LOADING){
-                val layout= binding.animationView
+        sharedPreferences = requireActivity().getSharedPreferences("login", Context.MODE_PRIVATE)
+        orderId = sharedPreferences.getInt("orderId", -1)
+
+        ///**********************************    (  call fun  )   **********************************
+
+        observeStatus()
+        setUserName()
+        callOrderRequest()
+        finalPayment()
+        setListOfProductId()
+        getEachProduct()
+        setRecyclerView()
+        setListOfQuantitty()
+
+        ///*****************************************************************************************
+
+    }
+
+    private fun setRecyclerView() {
+        produceList.observe(viewLifecycleOwner) {
+            val manager = LinearLayoutManager(requireContext())
+            binding.recyclerview.layoutManager = manager
+            val adapter = OrderAdapter(quantityLista,"", "", this) {  id , count , price -> updateCart(id , count, price) }
+            adapter.submitList(it)
+            Log.d("ppprrr", it.toString())
+            binding.recyclerview.adapter = adapter
+
+            it.forEach { i ->
+                total += i.price.toBigDecimal()
+            }
+        }
+    }
+
+    private fun updateCart(id: Int , count: Int, price :BigDecimal) {
+        total += price
+        var lineItem = LineItem(id , count)
+        var order = OrderResponse(orderId, listOf(lineItem))
+        cartViewModel.updateCart(order, orderId)
+    }
+
+    private fun getEachProduct() {
+        for (productId in finalIdList) {
+            cartViewModel.getItemDetail(productId)
+        }
+
+        cartViewModel.produceItemLiveData.observe(viewLifecycleOwner) {
+            produceList.value?.plus(it)
+            val list = produceList.value?.toMutableList()
+            list?.add(it)
+            produceList.value = list!!
+            Log.d("ppplivedata", produceList.value.toString())
+        }
+    }
+
+    private fun setListOfProductId() {
+        val productIdList = sharedPreferences.getString("productIdList", "")
+        var a = productIdList?.split(",")
+        for (i in 0..a!!.size - 2) {
+            finalIdList.add(a[i].toInt())
+        }
+        Log.d("pppp", finalIdList.toString())
+    }
+
+    private fun setListOfQuantitty() {
+        val quantityList = sharedPreferences.getString("quantityList", "")
+        var a = quantityList?.split(",")
+        for (i in 0..a!!.size - 2) {
+            quantityLista.add(a[i].toInt())
+        }
+        Log.d("pppp", quantityLista.toString())
+    }
+
+    private fun finalPayment() {
+        binding.button.setOnClickListener {
+
+            AlertDialog.Builder(requireContext())
+                .setTitle("Total Price : ")
+                .setMessage(total.toString())
+                .setNegativeButton("cancel") { _, _ -> }
+                .setPositiveButton("ok") { _, _ -> clearCart() }
+                .setCancelable(false)
+                .show()
+        }
+    }
+
+    private fun callOrderRequest() {
+        cartViewModel.getMyOrder(orderId)
+    }
+
+    private fun setUserName() {
+        val name = sharedPreferences.getString("name", "")
+        binding.name.text = name
+    }
+
+    private fun observeStatus() {
+        cartViewModel.status.observe(viewLifecycleOwner) {
+            if (it == ApiStatus.LOADING) {
+                val layout = binding.animationView
                 layout.isGone = false
                 binding.line1.isGone = true
-            }
-            else{
-                val layout= binding.animationView
+            } else {
+                val layout = binding.animationView
                 layout.isGone = true
                 binding.line1.isGone = false
             }
         }
+    }
 
-        sharedPreferences = requireActivity().getSharedPreferences("login", Context.MODE_PRIVATE)
-        var customerId = sharedPreferences.getInt("id", -1)
-        var name = sharedPreferences.getString("name", "")
-        var orderId = sharedPreferences.getInt("orderId", -1)
+    private fun clearCart() {
+        cartViewModel.deleteOrder(orderId)
+        var order = OrderResponse(0, listOf())
+        cartViewModel.addToCart(order)
 
-        binding.name.text = name
-        cartViewModel.getMyOrder(orderId)
-        cartViewModel.orderListLiveData.observe(viewLifecycleOwner){
+        cartViewModel.orderLiveData.observe(viewLifecycleOwner) {
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putInt("orderId", it.id)
+            requireContext().getSharedPreferences("login", Context.MODE_PRIVATE).edit().remove("productIdList").commit();
+//            editor.putString("productIdList", "")
+//            editor.putString("quantityList", "")
+            editor.apply()
 
-            if (it.line_items.size == 0) {
-                Toast.makeText(requireContext(),"empty", Toast.LENGTH_SHORT).show()
-            }
-            val manager = LinearLayoutManager(requireContext())
-            binding.recyclerview.layoutManager = manager
-            var adapter = OrderAdapter("","",this) {  }
-            adapter.submitList(it.line_items)
-            binding.recyclerview.setAdapter(adapter)
+
+
         }
+
+
+        val manager = LinearLayoutManager(requireContext())
+        binding.recyclerview.layoutManager = manager
+        val adapter = OrderAdapter(quantityLista,"", "", this) {  id , count , price -> updateCart(id , count, price) }
+        adapter.submitList(arrayListOf())
+//        Log.d("ppprrr", it.toString())
+        binding.recyclerview.adapter = adapter
+
+        total = a.toBigDecimal()
     }
 }
