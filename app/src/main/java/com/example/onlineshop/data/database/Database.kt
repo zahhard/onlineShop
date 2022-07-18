ae3faa87c0ea03fd1b254d6745015b87a92cc24e
zahhard <za.darardoust@gmail.com>
2022-07-18 16:41:49 +0430

    imp : database added

---
+++ app/src/main/java/com/example/onlineshop/data/database/Database.kt
@@ -0,0 +1,10 @@
+package com.example.onlineshop.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+//@Database(entities = [OrderId::class,Address::class], version = 1)
+//abstract class OrderDataBase : RoomDatabase(){
+//    abstract val orderDao:OrderDao
+//    abstract val addressDao :AddressDao
+//}
